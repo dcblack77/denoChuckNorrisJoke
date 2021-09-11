aef6a6ac6f75dd263f61d8daee818cec3fd01273
Diego Cabrera <diegocabrera123@gmail.com>
2021-09-11 22:18:26 +0200

    execute directly class

--- index.ts
+++ index.ts
@@ -1,23 +1,13 @@
-class DenoApp {
-    
+(new class DenoApp {
+
     constructor() {
         this.getChuckRandomJoke();
     }
 
 
     async getChuckRandomJoke() {
-        const joke = await fetch("https://api.chucknorris.io/jokes/random");
+        const joke=await fetch("https://api.chucknorris.io/jokes/random");
         console.log(joke.status);
         console.log(await joke.json());
     }
-
-    randomNumber(caracters: number) {
-        const numberArr = [];
-        for (let index = 0; index < caracters; index++) {
-            numberArr.push(Math.random());
-        }
-        return numberArr;
-    }
-}
-
-new DenoApp();
+}());
